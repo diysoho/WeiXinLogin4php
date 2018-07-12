30989a8053239b16907d4f4f0d34422c4f99ff1c
小虎牙 <lzs.haier@qq.com>
2018-07-12 15:57:09 +0800

    update to date

--- wxconfig.php
+++ wxconfig.php
@@ -1,5 +1,5 @@
 <?php
 ###微信公众号AppID
-define("__WXAPPID__","wx72b6c4a91776953f");
+define("__WXAPPID__","");
 ###微信公众号AppSecret
-define("__WXAPPSECRET__","0ed7d12242bb28ebcf06eba3e3bab458");
+define("__WXAPPSECRET__","");
